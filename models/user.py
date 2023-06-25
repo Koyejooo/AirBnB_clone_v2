45d108c0866ed0e127fb6c77b66bb3a2eed426f6
Koyejo <adinlewakoyejo@yahoo.com>
2023-06-24 18:10:58 -0700

    Updated to make Class an SQL table representation

--- models/user.py
+++ models/user.py
@@ -5,15 +5,18 @@ from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
-class User(BaseModel):
-    """This class defines a user by various attributes"""
+class User(BaseModel, Base):
+    """Represent and link to a MySQL table - "users",
+    with the table columns represented as class attributes:
+        a. email - A column of a str of max 128 chars; CANNOT be null.
+        b. password - A column of a str of max 128 chars; CANNOT be null.
+        c. first_name - A column of a str of max 128 chars; CAN be null.
+        d. last_name - A column of a str of max 128 chars; CAN be null.
+    """
     __tablename__ = 'users'
 
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
-    places = relationship("Place", cascade="all, delete", backref="user")
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    # places = relationship("Place", cascade="all, delete", backref="user")
