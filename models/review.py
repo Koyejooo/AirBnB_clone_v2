37c056178070a5d8c5543c69d7cb0e7f1ec6a401
Koyejo <adinlewakoyejo@yahoo.com>
2023-06-24 19:15:08 -0700

    Updated to make Class an SQL table representation

--- models/review.py
+++ models/review.py
@@ -1,10 +1,20 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from models.place import Place
+from models.user import User
+from sqlalchemy import String, ForeignKey
 
 
-class Review(BaseModel):
-    """ Review classto store review information """
-    place_id = ""
-    user_id = ""
-    text = ""
+class Review(BaseModel, Base):
+    """Represent and link to a MySQL table - "reviews",
+    with the table columns represented as class attributes:
+        a. text - A column for a str of max 1024 chars; CANNOT be null.
+        a. place_id(FK) - A column for a str of max 60 chars; CANNOT be null.
+        b. user_id(FK) - A column for a str of max 60 chars; CANNOT be null.
+    """
+    __tablename__ = 'reviews'
+
+    place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    text = Column(String(1024), nullable=False)
